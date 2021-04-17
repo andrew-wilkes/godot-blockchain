d6d4b51b307cefcabaa274d010cd99da378696a2
andrew-wilkes <github@urgentclick.com>
2021-04-17 16:08:58 +0000

    Add validate placeholder func

--- src/Blockchain.gd
+++ src/Blockchain.gd
@@ -1,3 +1,8 @@
 extends Control
 
 class_name Blockchain	
+
+func validate() -> bool:
+	# work back through the blocks to confirm that the previous_hash
+	# values match the computed hash of each block
+	return true
