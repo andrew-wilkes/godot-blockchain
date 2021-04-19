640fd682101ba6a6be8498719f31058d0cbc884c
andrew-wilkes <github@urgentclick.com>
2021-04-19 23:27:30 +0000

    Fix warning message

--- src/Network.gd
+++ src/Network.gd
@@ -8,6 +8,7 @@ var validators: Array
 func pick_validator() -> Validator:
 	var total_stakes = 0
 	var stakes = []
+	var the_chosen_one: Validator
 	for v in validators:
 		var validator: Validator = v
 		total_stakes += validator.stake
@@ -16,4 +17,6 @@ func pick_validator() -> Validator:
 	for i in stakes.size():
 		pick -= stakes[i]
 		if pick < 0:
-			return validators[i]
+			the_chosen_one = validators[i]
+			break
+	return the_chosen_one
